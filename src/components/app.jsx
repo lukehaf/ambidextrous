c3e1160f6f9ead4e400dd558ecd66fbcaa6509a1
lukehaf <Luke.D.Hafermann.25@Dartmouth.edu>
2024-12-28 23:24:27 -0800

    added routing support

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import '../style.scss';
 
 // import our components
 import Counter from './counter';
 import Controls from './controls';
+import NavbarAndPage from './routing.jsx';
 
 function App(props) {
   return (
     <div>
       <Counter />
       <Controls />
+      <NavbarAndPage />
     </div>
   );
 };
